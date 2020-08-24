a095aaf88961d5bcb62f91f4a7dff6b75c7ec7b2
yanggit2019 <838067203@qq.com>
2020-08-24 22:13:25 +0800

    cache的原理

--- src/main/scala/com/hainiu/sparkcore/SparkCacheDemo.scala
+++ src/main/scala/com/hainiu/sparkcore/SparkCacheDemo.scala
@@ -17,6 +17,9 @@ object SparkCacheDemo{
         (f, 1)
       }
     )
+    //因为cache是把rdd的数据存储到存储内存，当执行后续操作的时候，可以直接从存储内存拿取数据
+    //进而达到数据的复用
+    //cache 默认的缓存级别是StorageLevel.MEMORY_ONLY(完全放入内存)
     val cache: rdd2.type = rdd2.cache()
     cache.count()
     cache.count()
