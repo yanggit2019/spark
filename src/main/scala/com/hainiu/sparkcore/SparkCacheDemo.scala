fc4bc7f476dff9e1c4e4178a77fcb48e6b1c8fa5
yanggit2019 <838067203@qq.com>
2020-08-24 21:59:46 +0800

    spark cache 应用

---
+++ src/main/scala/com/hainiu/sparkcore/SparkCacheDemo.scala
@@ -0,0 +1,22 @@
+package com.hainiu.sparkcore
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object SparkCacheDemo{
+    
+  def main(args: Array[String]): Unit = {
+  //local[*]:利用cpu核数运算
+  //local[N]:利用N个核数运算
+  val sparkConf: SparkConf = new SparkConf().setAppName("SparkCacheDemo").setMaster("local[*]")
+  val sc = new SparkContext(sparkConf)
+  val rdd: RDD[String] = sc.textFile("H:\\input1")
+    val rdd2: RDD[(String, Int)] = rdd.flatMap(_.split("\t")).map(
+      f => {
+        println(s"f:${f}")
+        (f, 1)
+      }
+    )
+    rdd2.count()
+  }
+}
