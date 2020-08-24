d54f1cf48837ecccb3530bcbffd8f021a90830db
yanggit2019 <838067203@qq.com>
2020-08-24 21:20:27 +0800

    结果输出到hdfs

--- src/main/scala/com/hainiu/sparkcore/WordCount.scala
+++ src/main/scala/com/hainiu/sparkcore/WordCount.scala
@@ -1,5 +1,9 @@
 package com.hainiu.sparkcore
 
+
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
@@ -14,8 +18,20 @@ object WordCount {
     //因为groupBy操作是按照key把value聚合在一起，但并不运算，这样拉取的数据量就很大
     //所以建议用reduceByKey,它是按照key进行聚合，并运算，那拉取的数据是已经计算完的结果，效率高
     val resRdd: RDD[(String, Int)] = rdd.flatMap(_.split("\t")).map((_, 1)).reduceByKey(_+_)
-    val arr: Array[(String, Int)] = resRdd.collect()
-    println(arr.toBuffer)
+//    val arr: Array[(String, Int)] = resRdd.collect()
+//    println(arr.toBuffer)
+    //将内容写入文件
+    val res2: RDD[String] = resRdd.map(f => s"${f._1}\t${f._2}")
+    val outputDir = "/tmp/spark/output"
+    val hadoopConf = new Configuration
+    val fs:FileSystem = FileSystem.get(hadoopConf)
+    val outputPath = new Path(outputDir)
+    if (fs.exists(outputPath)){
+      fs.delete(outputPath,true)
+      println(s"delete outputpath:[${outputDir} success!]")
+    }
+    //把rdd数据写入到hdfs上
+    res2.saveAsTextFile(outputDir)
     
   }
 }
