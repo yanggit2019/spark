85ae06bf18bbff5c08ca04a0f22a588c58ec6f76
yanggit2019 <838067203@qq.com>
2020-08-25 23:44:11 +0800

    只加checkpoin

--- src/main/scala/com/hainiu/sparkcore/CheckPointDemo.scala
+++ src/main/scala/com/hainiu/sparkcore/CheckPointDemo.scala
@@ -7,11 +7,15 @@ object CheckPointDemo {
   def main(args: Array[String]): Unit = {
     val conf: SparkConf = new SparkConf().setMaster("local[5]").setAppName("CheckPointDemo")
     val sc = new SparkContext(conf)
+    //设置checkpoint目录
+    sc.setCheckpointDir("/tmp/spark/check_point_demo_data")
     val rdd: RDD[Int] = sc.parallelize(1 to 3, 2)
     val rdd2: RDD[Int] = rdd.map(f => {
       println(s"f:${f}")
       f * 10
     })
-    rdd2.count()
+    //checkpoint是转换算子
+    val cp: Unit = rdd2.checkpoint()
+//    rdd2.count()
   }
 }
