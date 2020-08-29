828c4200f61ed490f1a435a03912a4afa8289edd
yanggit2019 <838067203@qq.com>
2020-08-29 10:16:24 +0800

    HBase writePartition

--- src/main/scala/com/hainiu/hbase/SparkHbase4WritePartit.scala
+++ src/main/scala/com/hainiu/hbase/SparkHbase4WritePartit.scala
@@ -10,13 +10,13 @@ import org.apache.hadoop.mapreduce.Job
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
-object SparkHbase3Write {
+object SparkHbase4WritePartit {
   def main(args: Array[String]): Unit = {
-    val sparkConf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("SparkHbase3Write")
+    val sparkConf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("SparkHbase4WritePartition")
     val sc = new SparkContext(sparkConf)
-    val rdd: RDD[Int] = sc.parallelize(30 until 40, 2)
+    val rdd: RDD[Int] = sc.parallelize(50 until 99, 20)
     val writeHbaseRdd: RDD[(NullWritable, Put)] = rdd.map(f => {
-      val put = new Put(Bytes.toBytes(s"spark_write_${f}"))
+      val put = new Put(Bytes.toBytes(s"spark_write_p_${f}"))
       put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("count"), Bytes.toBytes(s"${f}"))
       (NullWritable.get(), put)
     }
@@ -32,7 +32,10 @@ object SparkHbase3Write {
     //设置输出的valueclass
     job.setOutputValueClass(classOf[Put])
     
-    
+    println(s"修改分区前分区数:${writeHbaseRdd.getNumPartitions}")
+    //后面Spark SQL的groupby操作会产生200个分区，可以通过coalesce减少分区数
+    val coalesceRdd: RDD[(NullWritable, Put)] = writeHbaseRdd.coalesce(2)
+    println(s"修改分区后分区数:${coalesceRdd.getNumPartitions}")
     //一个分区创建一个连接，一条一条写入
     writeHbaseRdd.saveAsNewAPIHadoopDataset(job.getConfiguration)
   }
