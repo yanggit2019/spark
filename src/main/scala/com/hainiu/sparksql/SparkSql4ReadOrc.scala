b27af86f64ef5b72fd42e2d540d048d14cd37967
yanggit2019 <838067203@qq.com>
2020-08-31 22:15:11 +0800

    过滤大于5的城市

--- src/main/scala/com/hainiu/sparksql/SparkSql4ReadOrc.scala
+++ src/main/scala/com/hainiu/sparksql/SparkSql4ReadOrc.scala
@@ -1,11 +1,14 @@
 package com.hainiu.sparksql
 
-import org.apache.spark.sql.{DataFrame, SQLContext}
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SQLContext}
 import org.apache.spark.{SparkConf, SparkContext}
 
 object SparkSql4ReadOrc {
   def main(args: Array[String]): Unit = {
     val conf: SparkConf = new SparkConf().setAppName("SparkSql4ReadOrc").setMaster("local[*]")
+    
+    //设置默认的shuffle产生的分区数
+    conf.set("spark.sql.shuffle,partitions","1")
     val sc = new SparkContext(conf)
     
     //根据SparkContext创建SQLContext
@@ -14,5 +17,12 @@ object SparkSql4ReadOrc {
     val df: DataFrame = sqlc.read.orc("H:\\input_orc")
     df.printSchema()
     df.show()
+    //select country,num from
+    //(select country,count(*) as num from XXXX group by country) t
+    //where t.num > 5
+    val groupByDF: DataFrame = df.groupBy("country").count()
+    val filterDF: Dataset[Row] = groupByDF.filter(groupByDF("count") > 5)
+    filterDF.printSchema()
+    filterDF.show()
   }
 }
