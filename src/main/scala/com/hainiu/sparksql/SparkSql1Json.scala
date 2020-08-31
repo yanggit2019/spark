1425932e97392f5bf8b0b6333a72adfdf334ae40
yanggit2019 <838067203@qq.com>
2020-08-31 20:30:12 +0800

    将统计结果保存到hdfs上

--- src/main/scala/com/hainiu/sparksql/SparkSql1Json.scala
+++ src/main/scala/com/hainiu/sparksql/SparkSql1Json.scala
@@ -1,8 +1,9 @@
 package com.hainiu.sparksql
 
-import org.apache.spark.sql.{DataFrame, SQLContext}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{DataFrame, Row, SQLContext}
 import org.apache.spark.{SparkConf, SparkContext}
-
+import com.hainiu.util.MyPredef.string2HDFSUtil
 object SparkSql1Json {
   def main(args: Array[String]): Unit = {
     val conf: SparkConf = new SparkConf().setAppName("SparkSql1Json").setMaster("local[*]")
@@ -23,5 +24,21 @@ object SparkSql1Json {
     df.select(df("country"),(df("num")+1).as("num1")).show()
     //查询num<2的数据
     df.filter(df("num")<2).show()
+    //按照country统计相同country的数量
+    //select country,count(*) from XXX group by country
+    val groupByDF: DataFrame = df.groupBy("country").count()
+    groupByDF.printSchema()
+    groupByDF.show()
+    //将统计后的结果保存到hdfs上
+    //DataFrame可以直接转rdd，而且转完是rdd[Row]
+    val rdd: RDD[Row] = groupByDF.rdd
+    //rdd[Row] --> rdd[String] String 里是“CN 2”
+    val rdd2: RDD[String] = rdd.map(row => {
+      s"${row.getString(0)}\t${row.getLong(1)}"
+    })
+    
+    val outputDir:String = "/tmp/sparksql/output_json"
+    outputDir.deleteHdfs()
+    rdd2.saveAsTextFile(outputDir)
   }
 }
