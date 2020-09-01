6524e58df73de90559742b8fb4edc66a7a985f72
yanggit2019 <838067203@qq.com>
2020-09-01 16:45:39 +0800

    spark session 执行sql语句

--- src/main/scala/com/hainiu/sparksql/SparkSqlMysqlSession.scala
+++ src/main/scala/com/hainiu/sparksql/SparkSqlMysqlSession.scala
@@ -1,6 +1,7 @@
 package com.hainiu.sparksql
 
-import org.apache.spark.sql.SparkSession
+import com.mysql.jdbc.Driver
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object SparkSqlMysqlSession {
   def main(args: Array[String]): Unit = {
@@ -15,15 +16,19 @@ object SparkSqlMysqlSession {
       .config("spark.sql.shuffle.partitions", "1")
       .config("spark.master", "local[*]")
       .appName("SparkSqlMysqlSession").getOrCreate()
+
+    val data: DataFrame = session.read.format("jdbc")
+      .option("driver", classOf[Driver].getName)
+      .option("url", "jdbc:mysql://nn2.hadoop:3306/hainiucralwer")
+      .option("dbtable", "hainiu_web_seed")
+      .option("user", "hainiu")
+      .option("password", "12345678").load()
     
+    data.createOrReplaceTempView("tmp")
+
+    val row: DataFrame = session.sql("select host from tmp")
     
-    
-    
-    
-    
-    
-    
-    
+    row.show()
     
     
   }
