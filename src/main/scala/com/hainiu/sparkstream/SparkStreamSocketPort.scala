7a40f219b2b70c7c8afe4f3285f1206ad2ad0b10
yanggit2019 <838067203@qq.com>
2020-09-01 21:51:18 +0800

    直接使用foreachRdd实现

--- src/main/scala/com/hainiu/sparkstream/SparkStreamSocketPort.scala
+++ src/main/scala/com/hainiu/sparkstream/SparkStreamSocketPort.scala
@@ -11,21 +11,28 @@ object SparkStreamSocketPort {
     val streamingContext = new StreamingContext(conf, Durations.seconds(5))
 
     val lines: ReceiverInputDStream[String] = streamingContext.socketTextStream("192.168.1.51", 6666)
-
-    val flatMap: DStream[String] = lines.flatMap(_.split(" "))
-    val reduceBykey: DStream[(String, Int)] = flatMap.transform(r => {
-      val mapToPair: RDD[(String, Int)] = r.map((_, 1))
-      val reduceByKey: RDD[(String, Int)] = mapToPair.reduceByKey(_ + _)
-      reduceByKey
-    })
-//    val mapToPair: DStream[(String, Int)] = flatMap.map((_, 1))
-//    val reduceByKey: DStream[(String, Int)] = mapToPair.reduceByKey(_ + _)
-
-    reduceBykey.foreachRDD( r=>{
+    lines.foreachRDD(r =>{
       if (!r.isEmpty()){
-      println(s"${r.collect().toList}")
+      val flatMap: RDD[String] = r.flatMap(_.split(" "))
+      val mapToPair: RDD[(String, Int)] = flatMap.map((_, 1))
+      val reduceByKey: RDD[(String, Int)] = mapToPair.reduceByKey(_ + _)
+      println(s"${reduceByKey.collect().toList}")
       }
     })
+//    val flatMap: DStream[String] = lines.flatMap(_.split(" "))
+//    val reduceBykey: DStream[(String, Int)] = flatMap.transform(r => {
+//      val mapToPair: RDD[(String, Int)] = r.map((_, 1))
+//      val reduceByKey: RDD[(String, Int)] = mapToPair.reduceByKey(_ + _)
+//      reduceByKey
+//    })
+////    val mapToPair: DStream[(String, Int)] = flatMap.map((_, 1))
+////    val reduceByKey: DStream[(String, Int)] = mapToPair.reduceByKey(_ + _)
+//
+//    reduceBykey.foreachRDD( r=>{
+//      if (!r.isEmpty()){
+//      println(s"${r.collect().toList}")
+//      }
+//    })
     
     streamingContext.start()
     streamingContext.awaitTermination()
