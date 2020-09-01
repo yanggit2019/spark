bbe0b7d23fde7b1e1d39431508319fa0943aee00
yanggit2019 <838067203@qq.com>
2020-09-01 16:36:15 +0800

    spark session 第二种创建方式

--- src/main/scala/com/hainiu/sparksql/SparkSqlMysqlSession.scala
+++ src/main/scala/com/hainiu/sparksql/SparkSqlMysqlSession.scala
@@ -1,17 +1,20 @@
 package com.hainiu.sparksql
 
-import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 
 object SparkSqlMysqlSession {
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf()
-    conf.setAppName("SparkSqlMysqlSession")
-    conf.setMaster("local[*]")
-    conf.set("spark.sql.shuffle.partitions","1")
+//    val conf = new SparkConf()
+//    conf.setAppName("SparkSqlMysqlSession")
+//    conf.setMaster("local[*]")
+//    conf.set("spark.sql.shuffle.partitions","1")
+//
+//    val session: SparkSession = SparkSession.builder().config(conf).getOrCreate()
 
-    val session: SparkSession = SparkSession.builder().config(conf).getOrCreate()
-    
+    val session: SparkSession = SparkSession.builder()
+      .config("spark.sql.shuffle.partitions", "1")
+      .config("spark.master", "local[*]")
+      .appName("SparkSqlMysqlSession").getOrCreate()
     
     
     
