07257eb38a6d732cedb23cb373d700108ee88930
yanggit2019 <838067203@qq.com>
2020-09-01 21:46:19 +0800

    transform 生成reduceByKey

--- src/main/scala/com/hainiu/sparkstream/SparkStreamSocketPort.scala
+++ src/main/scala/com/hainiu/sparkstream/SparkStreamSocketPort.scala
@@ -1,6 +1,7 @@
 package com.hainiu.sparkstream
 
 import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
 import org.apache.spark.streaming.dstream.{DStream, ReceiverInputDStream}
 import org.apache.spark.streaming.{Durations, StreamingContext}
 
@@ -12,10 +13,15 @@ object SparkStreamSocketPort {
     val lines: ReceiverInputDStream[String] = streamingContext.socketTextStream("192.168.1.51", 6666)
 
     val flatMap: DStream[String] = lines.flatMap(_.split(" "))
-    val mapToPair: DStream[(String, Int)] = flatMap.map((_, 1))
-    val reduceByKey: DStream[(String, Int)] = mapToPair.reduceByKey(_ + _)
-    
-    reduceByKey.foreachRDD( r=>{
+    val reduceBykey: DStream[(String, Int)] = flatMap.transform(r => {
+      val mapToPair: RDD[(String, Int)] = r.map((_, 1))
+      val reduceByKey: RDD[(String, Int)] = mapToPair.reduceByKey(_ + _)
+      reduceByKey
+    })
+//    val mapToPair: DStream[(String, Int)] = flatMap.map((_, 1))
+//    val reduceByKey: DStream[(String, Int)] = mapToPair.reduceByKey(_ + _)
+
+    reduceBykey.foreachRDD( r=>{
       if (!r.isEmpty()){
       println(s"${r.collect().toList}")
       }
